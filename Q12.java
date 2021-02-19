a893ed197b37ab73f03b0488e2bba2c7f4068b8f
Nitin <nitin.khandelwal@tothenew.com>
2021-02-19 10:28:57 +0530

    fixed Q12

--- Q12.java
+++ Q12.java
@@ -1,12 +1,12 @@
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class Q12 {
     public static void main(String[] args) {
         List<Integer> nums = List.of(15,89,45,5,6,74,8,35,1,2);
-        List<Integer> n =  nums.stream()
+            Optional<Integer> n =  nums.stream()
                 .filter(x -> x%2==0 & x>3 )
-                .collect(Collectors.toList());
-        System.out.println("even numbers greater than 3 are: " + n);
+                .findFirst();
+        System.out.println("first even number greater than 3 are: " + n);
     }
 }
